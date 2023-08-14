2d963a3a6d909ba57b8afaa4c974dc04768c7693
litarne <705290832@qq.com>
2023-08-14 18:58:55 +0800

    new code

---
+++ test1.py
@@ -0,0 +1,39 @@
+def calculate_tx_num_sum(filename, start_height, end_height):
+    total_tx_num = 0
+    in_target_range = False
+
+    with open(filename, 'r') as file:
+        for line in file:
+            if 'ProposeBlockTxs:' in line:
+                if 'block.height =' in line:
+                    parts = line.split('block.height =')
+                    if len(parts) > 1:
+                        height_str = parts[1].split()[0].strip()
+                        try:
+                            height = int(height_str)
+                            if start_height <= height <= end_height:
+                                in_target_range = True
+                            else:
+                                in_target_range = False
+                        except ValueError:
+                            pass
+
+                if in_target_range and 'tx num =' in line:
+                    parts = line.split('tx num =')
+                    if len(parts) > 1:
+                        tx_num_str = parts[1].strip()
+                        try:
+                            tx_num = int(tx_num_str)
+                            total_tx_num += tx_num
+                        except ValueError:
+                            pass
+
+    return total_tx_num
+
+
+filename = 'output.log'
+start_height = 36
+end_height = 79
+sum_tx_num = calculate_tx_num_sum(filename, start_height, end_height)
+print(
+    f"Sum of tx num from block.height {start_height} to {end_height}: {sum_tx_num}")
